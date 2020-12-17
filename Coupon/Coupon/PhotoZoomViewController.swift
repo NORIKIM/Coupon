a7de90a92b7b324cc0fb8a0fe2988e4ece564fb8
김지나 <jinagold.k@gmail.com>
2020-12-17 18:49:40 +0900

    PhotoZoomViewController 생성

---
+++ Coupon/Coupon/PhotoZoomViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoZoomViewController.swift
+//  Coupon
+//
+//  Created by 김지나 on 2020/12/17.
+//  Copyright © 2020 김지나. All rights reserved.
+//
+
+import UIKit
+
+class PhotoZoomViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
