b8cd78f8023e09b06aabc0855df0015ad8f49ca8
김지나 <jinagold.k@gmail.com>
2020-12-23 21:02:14 +0900

    스크롤뷰를 사용해 이미지뷰 확대 축소 설정

--- Coupon/Coupon/PhotoZoomViewController.swift
+++ Coupon/Coupon/PhotoZoomViewController.swift
@@ -8,20 +8,29 @@
 
 import UIKit
 
-class PhotoZoomViewController: UIViewController {
+class PhotoZoomViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var photo: UIImageView!
-    @IBOutlet var pinchGesture: UIPinchGestureRecognizer!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     var image: UIImage?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         photo.image = image
+        scrollViewSet()
     }
     
-    @IBAction func photoZoom(_ sender: Any) {
-        photo.isUserInteractionEnabled = true
-        photo.transform = photo.transform.scaledBy(x: pinchGesture.scale, y: pinchGesture.scale)
-        pinchGesture.scale = 1.0
+    // 확대할 뷰
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return photo
+    }
+    
+    func scrollViewSet() {
+        scrollView.alwaysBounceVertical = false
+        scrollView.alwaysBounceHorizontal = false
+        scrollView.minimumZoomScale = 1.0
+        scrollView.maximumZoomScale = 2.0
+        scrollView.delegate = self
     }
 }
