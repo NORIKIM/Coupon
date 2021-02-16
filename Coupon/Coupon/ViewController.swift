0a0a34af0fac7784ced81cf6978a37dbc875bcf8
김지나 <jinagold.k@gmail.com>
2021-02-16 22:32:25 +0900

    유효기간 확인 부 수정
    1. 코드 내 변수에 보여줄 유효기간 임박 쿠폰 인덱스를 저장하면 재실행 시 리셋 되므로 userDefaults를 이용하도록 수정
    2. 새로 추가된 쿠폰의 유효기간을 확인 하여 userDefaults에 저장하고 스크롤뷰 레이블에 반영되도록 함수 추가 및 정리

--- Coupon/Coupon/ViewController.swift
+++ Coupon/Coupon/ViewController.swift
@@ -7,61 +7,96 @@
 //
 
 import UIKit
-
+// 이거
 class ViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var pageControl: UIPageControl!
     
     let db = Database().readDB(select: "전체")
-    let page = 3
-    var dbRowCount = 0
-//    var expireCoupon = [Coupon]()
+    let userDefaults = UserDefaults.standard
+    let userDefaultsKey = "couponIndex"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        nearExpire()
-        
-//        if expireCoupon.count > 1 {
-            pageControl.numberOfPages = page//db.count
-//        }
         setCouponScreen()
     }
 
-    func setCouponScreen() {
-        let scrollwidth = self.scrollView.frame.width
-        let scrollheight = self.scrollView.frame.height
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
         
-        scrollView.contentSize = CGSize(width: CGFloat(page) * self.scrollView.frame.width, height: 0)
-        
-        for idx in 0 ..< page{//db.count {
-            let couponInfoLB = UILabel(frame: CGRect(x: CGFloat(idx) * scrollwidth , y: 0 , width: scrollwidth, height: scrollheight))
-            couponInfoLB.textAlignment = .center
-            couponInfoLB.text = "\(db[idx].category) \n \(db[idx].shop) \n \(db[idx].expireDate)"
-            couponInfoLB.numberOfLines = 3
-            
-            self.scrollView.addSubview(couponInfoLB)
-        }
-        scrollView.delegate = self
+        setCouponScreen()
     }
     
-    func nearExpire() {
-        print("near")
-        /*if db.count == 0 {
-            nearestExpireCoupon!.setTitle("등록된 쿠폰이 없습니다.:)", for: .normal)
-        } else if db.count == 1 {
-            let firstCoupon = db[0].shop
-            nearestExpireCoupon.setTitle(firstCoupon, for: .normal)
+    func findExpireCoupon() -> Void {
+        if db.count == 1 {
+            userDefaults.set([db.count - 1], forKey: userDefaultsKey)
+            return
         } else {
-            // 기간이 기존<신규 인지 비교(신규가 더 임박하냐?)
-            if db[dbRowCount].expireDate.compare(db[db.count-1].expireDate) == .orderedDescending {
-                dbRowCount = db.count - 1
-                nearestExpireCoupon.setTitle(db[dbRowCount].shop, for: .normal)
-            } else { // 기존=신규
-                
+            var couponIndexArr = userDefaults.object(forKey: userDefaultsKey) as! [Int]
+            let dateformatter = DateFormatter()
+            dateformatter.dateStyle = .long
+            let baseDate = dateformatter.string(from: db[couponIndexArr[0]].expireDate)
+            let addedDate = dateformatter.string(from: db[db.count-1].expireDate)
+            
+            if baseDate > addedDate {
+                userDefaults.set([db.count - 1], forKey: userDefaultsKey)
+                return
+            } else if baseDate == addedDate {
+                couponIndexArr.append(db.count - 1)
+                userDefaults.set(couponIndexArr, forKey: userDefaultsKey)
+                return
             }
-        }*/
+        }
+    }
+    
+    func clearScrollView() {
+        let scrollSubviews = self.scrollView.subviews
+        for subview in scrollSubviews {
+            subview.removeFromSuperview()
+        }
+    }
+    
+    // 임박한 쿠폰 보여주기
+    func setCouponScreen() {
+        guard let scroll = self.scrollView else {return}
+        let scrollwidth = scroll.frame.width
+        let scrollheight = scroll.frame.height
+        let couponIndexArr = userDefaults.object(forKey: userDefaultsKey) as? [Int] ?? [Int]()
         
+        if couponIndexArr.count == 1 || couponIndexArr.count == 0 {
+            pageControl.numberOfPages = 0
+        } else {
+            pageControl.numberOfPages = couponIndexArr.count
+        }
+        
+        if couponIndexArr.count == 0 {
+            let couponInfoLB = UILabel(frame: CGRect(x: 0 , y: 0 , width: scrollwidth, height: scrollheight))
+            couponInfoLB.textAlignment = .center
+            couponInfoLB.numberOfLines = 3
+            couponInfoLB.text = "등록된 쿠폰이 없습니다"
+            self.scrollView.addSubview(couponInfoLB)
+        } else {
+            clearScrollView()
+            
+            let dateFormat = DateFormatter()
+            dateFormat.dateFormat = "yyyy년 MM월 dd일 hh:mm a"
+
+            for idx in 0 ..< couponIndexArr.count {
+                let date = dateFormat.string(from: db[couponIndexArr[idx]].expireDate)
+                let couponInfoLB = UILabel(frame: CGRect(x: CGFloat(idx) * scrollwidth , y: 0 , width: scrollwidth, height: scrollheight))
+                couponInfoLB.textAlignment = .center
+                couponInfoLB.text = "\(db[couponIndexArr[idx]].category) \n \(db[couponIndexArr[idx]].shop) \n \(date)"
+                couponInfoLB.numberOfLines = 3
+                self.scrollView.addSubview(couponInfoLB)
+            }
+            scrollView.delegate = self
+        }
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let pageNumber = scrollView.contentOffset.x / scrollView.frame.size.width
+        pageControl.currentPage = Int(pageNumber)
     }
     
     // ListView에 선택한 카테고리의 타이틀을 listView로 넘겨줌
@@ -73,16 +108,6 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         }
         
         self.navigationController?.pushViewController(listView, animated: true)
-    }
-    
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-//        let pageNumber = scrollView.contentOffset.x / scrollView.frame.size.width
-//        pageControl.currentPage = Int(pageNumber)
-        
-        if fmod(scrollView.contentOffset.x, scrollView.frame.maxX) == 0 {
-            // Switch the location of the page.
-            pageControl.currentPage = Int(scrollView.contentOffset.x / scrollView.frame.maxX)
-        }
     }
 }
 
