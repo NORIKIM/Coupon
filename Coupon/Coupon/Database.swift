a30bb06138bef1b6cb519c9169bc075000ca7030
김지나 <jinagold.k@gmail.com>
2021-02-18 16:04:29 +0900

    데이터베이스에 이미지 저장 안되는 오류 수정

--- Coupon/Coupon/Database.swift
+++ Coupon/Coupon/Database.swift
@@ -82,16 +82,15 @@ struct Database {
     func insert(category: String, shop: String, price: String, expireDate: Date, content: String, contentPhoto: UIImage) {
         let insertQuery = "INSERT INTO coupon(category, shop, price, expireDate, content, contentPhoto) VALUES(?,?,?,?,?,?)"
         var insertStmt:OpaquePointer? = nil
-        let imgData = contentPhoto.pngData()
-        let img64 = imgData?.base64EncodedString(options: .lineLength64Characters)
-        
+        let imgData = contentPhoto.pngData()! as NSData
+
         if sqlite3_prepare_v2(db, insertQuery, -1, &insertStmt, nil) == SQLITE_OK {
             sqlite3_bind_text(insertStmt, 1, (category as NSString).utf8String, -1, nil)
             sqlite3_bind_text(insertStmt, 2, (shop as NSString).utf8String, -1, nil)
             sqlite3_bind_text(insertStmt, 3, (price as NSString).utf8String, -1, nil)
             sqlite3_bind_double(insertStmt, 4, expireDate.timeIntervalSinceReferenceDate)
             sqlite3_bind_text(insertStmt, 5, (content as NSString).utf8String, -1, nil)
-            sqlite3_bind_blob(insertStmt, 6, img64, -1, nil)
+            sqlite3_bind_blob(insertStmt, 6, imgData.bytes, Int32(imgData.length), nil)
             
             if sqlite3_step(insertStmt) == SQLITE_DONE {
                 print("SUCCESS")
