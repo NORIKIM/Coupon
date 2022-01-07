70f8bf09ff405b7bfded572457288066e93ae8f8
Jina Kim <jinagold.k@gmail.com>
2022-01-07 14:49:03 +0900

    컬러 지정 삭제

--- Coupon/Coupon/AppDelegate.swift
+++ Coupon/Coupon/AppDelegate.swift
@@ -14,7 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        UIBarButtonItem.appearance().setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.systemPurple, NSAttributedString.Key.font: UIFont(name: "NanumSquareRoundR", size: 17)!], for: UIControl.State.normal)
 
         return true
     }
