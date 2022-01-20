69dbce05e14168020015953bc566d11dbefb2401
김지나 <jinagold.k@gmail.com>
2022-01-20 21:30:48 +0900

    쿠폰 목록 셀 분리

---
+++ Coupon/Coupon/ListCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListCell.swift
+//  Coupon
+//
+//  Created by 김지나 on 2022/01/20.
+//  Copyright © 2022 김지나. All rights reserved.
+//
+
+import UIKit
+
+class ListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
